d958c209da8ced44cbcf41bd2ecbe2131eb18531
KNOOP <knuop@icloud.com>
2024-10-20 01:24:30 +0800

    Update __init__.py

--- custom_components/fun_asr/__init__.py
+++ custom_components/fun_asr/__init__.py
@@ -3,14 +3,31 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 DOMAIN = "fun_asr"
-
 PLATFORMS = (Platform.STT,)
 
-
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    config_entry.add_update_listener(async_update_options)
+
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
-    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+
+    return unload_ok
+
+async def async_update_options(hass: HomeAssistant, config_entry: ConfigEntry):
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    return True
