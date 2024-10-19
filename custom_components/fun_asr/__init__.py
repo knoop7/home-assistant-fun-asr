a331411bcb159f507ab7264adfe1e40ad02e4252
KNOOP <knuop@icloud.com>
2024-10-20 01:43:20 +0800

    Update __init__.py

--- custom_components/fun_asr/__init__.py
+++ custom_components/fun_asr/__init__.py
@@ -2,7 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "fun_asr"
+DOMAIN = "czsvatek_sensor"
 PLATFORMS = (Platform.STT,)
 
 async def async_setup(hass: HomeAssistant, config: dict):
